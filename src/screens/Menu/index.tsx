55c34a1f83a2174e7894e7365feb6c6139518ea5
ankushnarwal2022 <ankush.narwal.dev@webologixglobal.com>
2023-10-12 20:06:21 +0530

    getInventories api basic done

--- src/screens/Menu/index.tsx
+++ src/screens/Menu/index.tsx
@@ -1,11 +1,39 @@
 import React, {useState} from "react";
 import {Container,  H7, Insets, margin, padding} from "@WebologicsIndia/react-native-components";
 import {theme} from "../../config/theme";
-import {Pressable, StyleSheet} from "react-native";
+import {Pressable, ScrollView, StyleSheet, View} from "react-native";
 import HamburgerSVG from "../../assets/hamburger.svg";
+import {inventoryUrl} from "../../config/api";
+import dayjs from "dayjs";
 
 const TrackingDrawer = (props:any) => {
     const [insets] = useState(Insets.getInsets());
+    const [loding, setLoading] = useState(false);
+    const [inventoryData, setInventoryData] = useState([]);
+    const [total, setTotal] = useState();
+    const [error, setError] = useState("");
+    const getInventories = () => {
+        setLoading(true);
+        fetch(`${inventoryUrl}?page=1&results=10`).then((res) => {
+            if (res.status === 200){
+                res.json().then((data) => {
+                    console.log("data", data);
+                    setInventoryData(data.results);
+                    setTotal(data.total);
+                });
+            } else {
+                res.json().then((data) => {
+                    setError(data.message);
+                    setLoading(false);
+                });
+            }
+        }).finally(() => {
+            setLoading(false);
+        });
+    };
+    React.useEffect(() => {
+        getInventories();
+    }, []);
     return (
         <Container
             style={styles.container}
@@ -17,7 +45,30 @@ const TrackingDrawer = (props:any) => {
             headerColor={theme.Primary}
             bottom={insets.bottom}
         >
-            <H7 style={[padding.py3, {color: theme.PrimaryDark}]}>Total 0</H7>
+            <H7 style={[padding.py3, {color: theme.PrimaryDark}]}>Total {total}</H7>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                {
+                    inventoryData.length ?
+                        inventoryData.map((item:any) => {
+                            console.log(item);
+                            return(
+                                <View key={item._id} style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center"}}>
+                                    <View style={padding.pb3}>
+                                        <H7 style={{color: theme.PrimaryDark}}>Name</H7>
+                                        <H7 style={{color: theme.PrimaryDark}}>{item.itemType}</H7>
+                                        <H7 style={{color: theme.PrimaryDark}}>Tags</H7>
+                                        <H7 style={{color: theme.PrimaryDark}}>0</H7>
+                                    </View>
+                                    <View>
+                                        <H7 style={{color: theme.PrimaryDark}}>Created</H7>
+                                        <H7 style={{color: theme.PrimaryDark}}>{dayjs(item.createdAt).format("DD-MMM-YYYY : HH-MM-A")}</H7>
+                                    </View>
+                                </View>
+                            );
+                        }) :
+                        <H7>No Data found</H7>
+                }
+            </ScrollView>
         </Container>
     );
 };
